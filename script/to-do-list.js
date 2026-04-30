17a647385190976b763fae8748952aba904b4371
Siminder Bansal <bansalsiminder234@gmail.com>
2026-04-30 17:16:46 +0000

    added functions for progress bar added updated and reset fucntion claacuted based on li and what li was strike threw

--- script/to-do-list.js
+++ script/to-do-list.js
@@ -4,7 +4,8 @@
 const input = document.getElementById("input");
 const list = document.getElementById("list");
 const ul = document.querySelector('ul');
-const progressBar = document.querySelector(".progressBar");
+const progressBar = document.querySelector(".progress");
+
 
 
 // get value from input 
@@ -16,6 +17,7 @@ input.addEventListener('keypress', function (e) {
         newli.textContent = input.value;
         newli.classList.add("liStyles")
         list.appendChild(newli);
+        resetProgress();
     }
 
    
@@ -25,16 +27,41 @@ input.addEventListener('keypress', function (e) {
 
  ul.addEventListener('click', function (e) {
     const liItem = e.target.closest('li')
-    if(liItem.tagName === 'LI') {
+    if(liItem && liItem.tagName === 'LI') {
         liItem.classList.toggle("strikeThrew")
 
+
+
+        updateProgressBar();
     }
+
+  
+    
+    
+    
 })
 
 
+function updateProgressBar() {
+    const tasks = document.querySelectorAll('#list li');
+    const completed = document.querySelectorAll('#list li.strikeThrew')
+
+    const percent = tasks.length === 0 ? 0 : (completed.length / tasks.length) * 100;
+
+    progressBar.style.width = percent + "%";
+    
+}
 
 
 
+function resetProgress() {
+    const tasks = document.querySelectorAll('#list li')
+    tasks.forEach(tasks => tasks.classList.remove('strikeThrew'));
+
+    progressBar.style.width = '0%';
+
+
+}
 
 // add it to a li element 
 // if li is clicked cross it out
